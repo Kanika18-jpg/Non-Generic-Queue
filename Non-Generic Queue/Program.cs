f0702fa2b5794474e4f8ee64a885de8202390718
hp <sabhrwalkanika1999@gmail.com>
2023-10-10 14:03:40 +0530

    Add project files.

---
+++ Non-Generic Queue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Non_Generic_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue("Hello");
+
+            foreach(var item in q)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(q.Count);
+            Console.WriteLine(q.Peek());
+            
+            q.Dequeue();
+
+            q.Enqueue("Welcome!");
+
+            foreach(var item in q)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(q.Contains(2));
+
+            Console.ReadLine();
+        }
+    }
+}
